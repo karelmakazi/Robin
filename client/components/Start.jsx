04fd211867ad6ef465d0a16bc66855b298bb03ff
Karel Fourie <karelmakazi@gmail.com>
2020-04-18 14:01:21 +1200

    Implement Start: variable direction and messaging

--- client/components/Start.jsx
+++ client/components/Start.jsx
@@ -2,37 +2,40 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import request from 'superagent'
 
-//DATA: Database - Check active Focus - stand-in variable
-const activeFocus = false
-
-//VARIABLE DIRECTION
-let buttonDirection = (activeFocus) ? "NEW SESSION" : "SET FOCUS"
-let routeComponent = (activeFocus) ? "/Session" : "/Focus"
-
-
-//VARIABLE MESSAGING
-const Active = " You have an active Focus for your practice. Enjoy your session."
-const noActive = " You do not currently have an active Focus set for your practice."
-const dynamicMessage  = (activeFocus) ? Active : noActive
 
 
 //STATEFUL COMPONENT
 class Start extends React.Component {
-
+  
   state = {
     activeCount: 0
   }
-
+  
   componentDidMount(){
     request.get('/api/v1/active')
-      .then(res => {
-        this.setState({
-          activeCount: res.body
-        })
+    .then(res => {
+      this.setState({
+        activeCount: res.body
       })
+    })
   }
-
+  
   render(){
+    
+    //DATA: Database - Check active Focus - stand-in variable
+    // const activeFocus = false
+    
+    //VARIABLE DIRECTION
+    let buttonDirection = (this.state.activeCount) ? "NEW SESSION" : "SET FOCUS"
+    let routeComponent = (this.state.activeCount) ? "/Session" : "/Focus"
+    
+    
+    //VARIABLE MESSAGING
+    const Active = " You have an active Focus for your practice. Enjoy your session."
+    const noActive = " You do not currently have an active Focus set for your practice."
+    const dynamicMessage  = (this.state.activeCount) ? Active : noActive
+    
+    
     return(
       <div className='appContainer'>
         <div className='mainImage'>
